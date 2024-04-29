375d3833280c58100cc84df446e6b170c8be422f
AISYAH MUHADDISI <aisyah.muhaddisi@btpn.com>
2024-04-30 00:42:25 +0700

    feat(todo): add chips

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const CHIPS_TITLE = ['ALL', 'ACTIVE', 'COMPLETE']
+
+const ChipItems = ({title, isActive, onClick, index}) => {
+    return (
+        <div className={`chip-items ${isActive && 'chip-items-active'}`} onClick={onClick(index)}>
+            <h5>
+                {title}
+            </h5>
+        </div>
+    )
+}
 
 const HomePage = ({
     params,
-}) => (
+}) => {
+    const [activeId, setActiveId] = useState(-1)
+
+    const onClick = (id) => () => {
+        if(id !== activeId) {
+            setActiveId(id)
+        }
+    };
+
+    return (
     <div className="center">
         <h3 className="title">What's the plan for today?</h3>
         <div className="add-todo-container">
@@ -10,12 +31,12 @@ const HomePage = ({
             <div className="add-todo-button">Add</div>
         </div>
         <div className="chip-container">
-            <div>
-                All
-            </div>
+            {CHIPS_TITLE.map((title, index) => {
+                return <ChipItems title={title} index={index} onClick={onClick} isActive={activeId === index}/>
+            })}
         </div>
     </div>
-);
+)};
 
 export default HomePage;
 
