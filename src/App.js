6ec0edc3305ba3cb8448c5b51db39b8347833c38
AISYAH MUHADDISI <aisyah.muhaddisi@btpn.com>
2024-05-03 09:46:59 +0700

    feat(todo): update package json

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/Home';
 
